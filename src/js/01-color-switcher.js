5c6e0c67d6349961436f625f01b607c91bd1f226
AnnaFutryk <anna.futryk@gmail.com>
2023-05-17 21:57:18 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnChangeColor);
+refs.stopBtn.addEventListener('click', onStopBtnChangeColor);
+
+function onStartBtnChangeColor() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+
+    refs.startBtn.disabled = true;
+};
+
+function onStopBtnChangeColor() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
